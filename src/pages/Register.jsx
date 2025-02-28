6b9c1f8082e2639105d35ba7c416f96190dde758
m0nster3nergy <casadoeinstein@gmail.com>
2025-02-28 07:07:20 -0300

    boilerplate

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -13,7 +13,7 @@ export default function Register() {
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
               alt="Your Company"
-              src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+              src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=yellow&shade=600"
               className="mx-auto h-10 w-auto"
             />
             <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white-900">
@@ -23,6 +23,21 @@ export default function Register() {
   
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form action="#" method="POST" className="space-y-6">
+            <div>
+                <label htmlFor="username" className="block text-sm/6 font-medium text-white-900">
+                  Username
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="username"
+                    name="username"
+                    type="username"
+                    required
+                    autoComplete="username"
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
               <div>
                 <label htmlFor="email" className="block text-sm/6 font-medium text-white-900">
                   Email address
@@ -44,11 +59,7 @@ export default function Register() {
                   <label htmlFor="password" className="block text-sm/6 font-medium text-white-900">
                     Password
                   </label>
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-yellow-500 hover:text-indigo-500">
-                      Forgot password?
-                    </a>
-                  </div>
+                  
                 </div>
                 <div className="mt-2">
                   <input
@@ -65,9 +76,9 @@ export default function Register() {
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-yellow-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="flex w-full justify-center rounded-md bg-yellow-500 px-3 py-1.5 text-sm/6 font-semibold text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Sign in
+                  Register
                 </button>
               </div>
             </form>
